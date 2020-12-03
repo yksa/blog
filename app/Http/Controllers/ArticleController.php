2e401934a907927c0643174e82e77756ffe44406
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-03 20:33:23 +0630

    article model added and article data to view

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return "Controller - Article List";
+        $data = Article::all();
+        return view('articles.index', [
+            'articles' => $data
+        ]);
     }
     public function detail($id)
     {
